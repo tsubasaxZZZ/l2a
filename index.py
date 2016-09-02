85584c7b5de2ca765d94f19e3b5360d678f19870
root <tsunomur@microsoft.com>
2016-09-03 04:41:18 +0900

    Fix

--- index.py
+++ index.py
@@ -4,4 +4,4 @@ from bottle import route, run, template
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
-run(host='0.0.0.0',port=2000, debug=True, reloader=True)
+run(host='0.0.0.0',port=2000, debug=True)
