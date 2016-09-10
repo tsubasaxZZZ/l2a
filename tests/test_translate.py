1bcc56cf66f8f5d334ae87fb95d61fa54ca2a60a
root <tsunomur@microsoft.com>
2016-09-10 20:01:03 +0900

    Add logging

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -6,9 +6,22 @@ from translates.translate import Translate
 from translates.translatebing import TranslateBing
 from nose.tools import *
 
+from logging import getLogger,StreamHandler,DEBUG,Formatter
+
 class TestTranslate:
+    def setup(self):
+        formatter = Formatter('%(asctime)s - %(name)s.%(funcName)s():%(lineno)s - %(levelname)s - %(message)s')
+
+        handler = StreamHandler()
+        handler.setLevel(DEBUG)
+        handler.setFormatter(formatter)
+
+        self.logger = getLogger(__name__)
+        self.logger.setLevel(DEBUG)
+        self.logger.addHandler(handler)
+
     def test_translatebing_en2ja(self):
-        bing = TranslateBing()
+        bing = TranslateBing(self.logger)
 
         t = bing.translate("Hello")
         print(t)
--- translates/translatebing.py
+++ translates/translatebing.py
@@ -2,10 +2,12 @@ from microsofttranslator import Translator
 
 class TranslateBing:
     
-    def __init__(self):
+    def __init__(self, logger):
         self.bing = Translator('l2a', '9W6zDb3DBJ77SCjWPOHZsp5y75j5gJvu0z0pjn9zdHk=')
+        self.logger = logger.getChild(__name__)
 
     def translate(self, source, to_lang=None):
+        self.logger.debug(source)
         if(not to_lang):
             to_lang = "ja" if self.bing.detect_language(source) == "en" else "en"
         return(self.bing.translate(source, to_lang))
